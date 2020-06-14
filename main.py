e99277bde0c5a4f2df671f7dcb54f5dfc24b5452
shecodessometimes <62902773+shecodessometimes@users.noreply.github.com>
2020-06-14 10:22:44 -0400

    Add files via upload
    updated files

--- main.py
+++ main.py
@@ -20,9 +20,11 @@ BLACK =      ( 23,  23,  23, 255.0)
 #########
 pg.font.init()
 myfont = pg.font.SysFont('Comic Sans MS', 120)
-myfont2 = pg.font.SysFont('VT323-Regular.ttf', 120)
-score_font = pg.font.SysFont('VT323-Regular.ttf', 60)
-tiny_score_font = pg.font.SysFont('VT323-Regular.ttf', 40)
+myfont2 = pg.font.Font("font/dosis.ttf", 120)
+score_font = pg.font.Font("font/dosis.ttf", 60)
+end_font = pg.font.Font("font/dosis.ttf", 55)
+tiny_score_font = pg.font.Font("font/dosis.ttf", 40)
+nyan_cat = pg.font.Font("font/bungeeinline.ttf", 60)
 
 #############
 # VARIABLES #
@@ -47,6 +49,7 @@ DONYANGIF = [scale_image(pg.image.load("images/donyan_gif/" + str(i+1) + ".png")
 
 NYANGIFS = [NYANGIF, GHOSTGIF, DONYANGIF]
 nyan_mode = 0
+donut_nyan_mode = 0
 nyangifcounter = 0
 
 ########
@@ -71,14 +74,14 @@ class Nyan(pg.sprite.Sprite):
         keys = pg.key.get_pressed()
         if not banana:
             if keys[pg.K_UP]:
-                self.rect.y -= 10
+                self.rect.y -= 3
             if keys[pg.K_DOWN]:
-                self.rect.y += 10
+                self.rect.y += 3
         else:
             if keys[pg.K_UP]:
-                self.rect.y += 10
+                self.rect.y += 3
             if keys[pg.K_DOWN]:
-                self.rect.y -= 10
+                self.rect.y -= 3
         if self.rect.right > SCREEN_WIDTH:
             self.rect.right = SCREEN_WIDTH
         if  self.rect.left < 0:
@@ -87,6 +90,44 @@ class Nyan(pg.sprite.Sprite):
                 self.rect.bottom = SCREEN_HEIGHT
         if  self.rect.top < 0:
                 self.rect.top = 0
+
+class Token_Nyan(pg.sprite.Sprite):
+    def __init__(self, given_x, given_y, given_nyan_mode, given_flip):
+        super().__init__()
+        self.x = given_x
+        self.y = given_y
+        self.image = NYANGIFS[0][0]
+        self.rect = self.image.get_rect(x=self.x, y=self.y)
+        self.flip = given_flip
+        nyan_mode = given_nyan_mode
+    def update(self):
+        global nyangifcounter, nyan_mode
+        nyangifcounter = nyangifcounter % len(NYANGIFS[nyan_mode])
+        if not self.flip:
+            self.image = NYANGIFS[nyan_mode][nyangifcounter]
+        else:
+            self.image = pg.transform.flip(NYANGIFS[nyan_mode][nyangifcounter],True, False)
+        nyangifcounter += 1
+        nyangifcounter = nyangifcounter % len(NYANGIFS[nyan_mode])
+
+class Donut_Nyan(pg.sprite.Sprite):
+    def __init__(self, given_x, given_y, given_nyan_mode, given_flip):
+        super().__init__()
+        self.x = given_x
+        self.y = given_y
+        self.image = NYANGIFS[0][0]
+        self.rect = self.image.get_rect(x=self.x, y=self.y)
+        self.flip = given_flip
+        donut_nyan_mode = given_nyan_mode
+    def update(self):
+        global nyangifcounter, donut_nyan_mode
+        nyangifcounter = nyangifcounter % len(NYANGIFS[donut_nyan_mode])
+        if not self.flip:
+            self.image = NYANGIFS[donut_nyan_mode][nyangifcounter]
+        else:
+            self.image = pg.transform.flip(NYANGIFS[donut_nyan_mode][nyangifcounter],True, False)
+        nyangifcounter += 1
+        nyangifcounter = nyangifcounter % len(NYANGIFS[donut_nyan_mode])
 
 # Cake Sprite - speeds up nyan cake
 class Cake(pg.sprite.Sprite):
@@ -104,7 +145,7 @@ class Cake(pg.sprite.Sprite):
             Speed += 10
             self.kill()
 
-# Cake Sprite - speeds up nyan cake
+# Donut Sprite - special powers 
 class Donut(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
@@ -172,30 +213,6 @@ class Asteroid(pg.sprite.Sprite):
         if collides(self):
             Speed -= self.speed
             self.kill()
-
-# # Pause button
-# class Pause(pg.sprite.Sprite):
-#     currently_clicked = False
-#     def __init__(self):
-#         pg.sprite.Sprite.__init__(self)
-#         self.image = scale_image(pg.image.load('images/pause.png'),1/6)
-#         self.rect = self.image.get_rect()
-#         self.rect.topright = (SCREEN_WIDTH,self.image.get_height())
-#     def update(self):
-#         if clicked(self):
-#             currently_clicked = True
-
-# # Play button
-# class Play(pg.sprite.Sprite):
-#     currently_clicked = True
-#     def __init__(self):
-#         pg.sprite.Sprite.__init__(self)
-#         self.image = scale_image(pg.image.load('images/play.png'),1/6)
-#         self.rect = self.image.get_rect()
-#         self.rect.topright = (SCREEN_WIDTH,self.image.get_height())
-#     def update(self):
-#         if clicked(self):
-#             currently_clicked = True
       
 def redrawWindow(Screen, bg, bgX, bgX2):
     Screen.blit(bg, (bgX, 0))
@@ -230,65 +247,131 @@ def game_loop(Screen):
     all_sprites.add(nyan)
 
     playing = True
+    paused = False
 
     while playing:
 
-        Score += Speed/100
-        
-        if (Speed > max_speed):
-            max_speed = Speed
+        #keep screen paused if space was pressed
+        keys = pg.key.get_pressed()
+        if keys[pg.K_SPACE]:
+            pg.time.delay(200)
+            if (not paused):
+                # speed_text = score_font.render('Too Speedy :/', False, WHITE)
+                # Screen.blit(speed_text,(200,200))
+                print("no u")
+                paused=True
+            elif (paused):
+                print("stuffu")
+                paused=False
 
-        number = random.random()
-        redrawWindow(Screen, bg, bgX, bgX2)
+        if not paused:
+            Score += Speed/100
+            if (Speed > max_speed):
+                max_speed = Speed
+                
+            number = random.random()
+            redrawWindow(Screen, bg, bgX, bgX2)
+            
+            bgX -= 2.8 * Speed
+            bgX2 -= 2.8 * Speed
+            
+            # If bg is at the -width then reset position
+            if bgX < bg.get_width() * -1:
+                bgX = bg.get_width()
+            
+            if bgX2 < bg.get_width() * -1:
+                bgX2 = bg.get_width()
 
-        bgX -= 2.8 * Speed
-        bgX2 -= 2.8 * Speed
+            time = pg.time.get_ticks()/1000
 
-        # If bg is at the -width then reset position
-        if bgX < bg.get_width() * -1:
-            bgX = bg.get_width()
-        
-        if bgX2 < bg.get_width() * -1:
-            bgX2 = bg.get_width()
+            if number < 1/500 and time > 0.5:
+                peel = Peel()
+                all_sprites.add(peel)	
+            if number < 1/2 and time > 1000:
+                donut = Donut()
+                all_sprites.add(donut)	
+            if number < 1/250 and time > 0.5 and not nyan_mode == 2:
+                cake = Cake()
+                all_sprites.add(cake)	
+            if number < 1/25 and time > 2:
+                asteroid = Asteroid()
+                all_sprites.add(asteroid)
 
-        time = pg.time.get_ticks()/1000
+            for event in pg.event.get():
+                if event.type == end_mode_2:
+                    nyan_mode = 0
 
-        if number < 1/500 and time > 0.5:
-            peel = Peel()
-            all_sprites.add(peel)	
-        if number < 1/2 and time > 1000:
-            donut = Donut()
-            all_sprites.add(donut)	
-        if number < 1/250 and time > 0.5 and not nyan_mode == 2:
-            cake = Cake()
-            all_sprites.add(cake)	
-        if number < 1/25 and time > 2:
-            asteroid = Asteroid()
-            all_sprites.add(asteroid)
-
-        for event in pg.event.get():
-            if event.type == end_mode_2:
-                nyan_mode = 0
-
-            if event.type == pg.QUIT:
-                playing = False
-                quit = True
+                if event.type == pg.QUIT:
+                    playing = False
+                    quit = True
            
+            all_sprites.update()
+            all_sprites.draw(Screen)
+
+            # display the score and the speed
+            score_text = score_font.render('Score: ' + str(int(Score)), False, WHITE)
+            Screen.blit(score_text,(0,0))
+        
+            speed_text = score_font.render('Speed: ' + str(Speed), False, WHITE)
+            Screen.blit(speed_text,(0,int(speed_text.get_height())))
+
+            #display 'press space to pause' text
+            space_to_pause_text = tiny_score_font.render('Press space to pause', False, WHITE)
+            Screen.blit(space_to_pause_text,(int(SCREEN_WIDTH/2-space_to_pause_text.get_width()/2),int((SCREEN_HEIGHT-1.5*space_to_pause_text.get_height()))))
+
+            pg.display.update()
+
+            # game over screen, max speed + score
+            if Speed <= 0:
+                playing = False
+
+def begin_card(Screen):
+    global myfont, nyan_mode, nyangifcounter, max_speed, Score, banana
+    ending = True
+    bg = pg.image.load('images/background.png').convert()
+    nyan_mode = 2
+    nyangifcounter = 1
+    banana = False
+    
+    nyan_the_colorful = Token_Nyan(175, 370, 0, False)
+    donut_monster = Donut_Nyan(525, 370, 2, True)
+
+    all_sprites = pg.sprite.Group()
+    all_sprites.add(nyan_the_colorful)
+    all_sprites.add(donut_monster)
+
+    quit = False
+
+    while ending:
+        Screen.blit(bg, (0, 0))
         all_sprites.update()
+        
+        rectangle_width = 600
+        rectangle_height = 350
+        pg.draw.rect(Screen,BLACK,(int((SCREEN_WIDTH-rectangle_width)/2),int((SCREEN_HEIGHT-rectangle_height)/2),rectangle_width,rectangle_height))
+        
+        space_between_text = SCREEN_HEIGHT/7
+        game_over_text = tiny_score_font.render('Welcome to the', False, WHITE)
+        Screen.blit(game_over_text,(int(SCREEN_WIDTH/2-game_over_text.get_width()/2),int(SCREEN_HEIGHT/2 - (space_between_text+game_over_text.get_height()))))
+
+        your_score_text = nyan_cat.render('NYAN UNIVERSE', False, (248, 24, 148))
+        Screen.blit(your_score_text,(int(SCREEN_WIDTH/2-your_score_text.get_width()/2),int(SCREEN_HEIGHT/2 - your_score_text.get_height()/2)))
+
+        try_again_text = tiny_score_font.render('Press s to start', False, WHITE)
+        Screen.blit(try_again_text,(int((SCREEN_WIDTH-try_again_text.get_width())/2),int(SCREEN_HEIGHT/2 + space_between_text)))
+
         all_sprites.draw(Screen)
 
-        # display the score and the speed
-        score_text = score_font.render('Score: ' + str(int(Score)), False, WHITE)
-        Screen.blit(score_text,(0,0))
-        
-        speed_text = score_font.render('Speed: ' + str(Speed), False, WHITE)
-        Screen.blit(speed_text,(0,int(speed_text.get_height())))
-
         pg.display.update()
-
-        # game over screen, max speed + score
-        if Speed <= 0:
-            playing = False
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                ending = False
+                quit = True
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_s:
+                    quit = False
+                    ending = False        
+    return quit
 
 def end_card(Screen):
     global myfont, nyan_mode, nyangifcounter, max_speed, Score, banana
@@ -307,18 +390,19 @@ def end_card(Screen):
         Screen.blit(bg, (0, 0))
         all_sprites.update()
         
-        rectangle_width = 650
-        rectangle_height = 400
+        rectangle_width = 600
+        rectangle_height = 350
         pg.draw.rect(Screen,BLACK,(int((SCREEN_WIDTH-rectangle_width)/2),int((SCREEN_HEIGHT-rectangle_height)/2),rectangle_width,rectangle_height))
         
-        game_over_text = myfont2.render('Game Over', False, WHITE)
-        Screen.blit(game_over_text,(int(SCREEN_WIDTH/2-game_over_text.get_width()/2),int(SCREEN_HEIGHT/2 - 3*game_over_text.get_height()/2)))
+        space_between_text = SCREEN_HEIGHT/7
+        game_over_text = end_font.render('Game Over', False, WHITE)
+        Screen.blit(game_over_text,(int(SCREEN_WIDTH/2-game_over_text.get_width()/2),int(SCREEN_HEIGHT/2 - (space_between_text+game_over_text.get_height()))))
 
-        your_score_text = tiny_score_font.render('Final Score: ' + str(int(Score)) + '           Max Speed: ' + str(max_speed), False, WHITE)
-        Screen.blit(your_score_text,(int(SCREEN_WIDTH/2-your_score_text.get_width()/2),int(SCREEN_HEIGHT/2 + your_score_text.get_height())))
+        your_score_text = tiny_score_font.render('Final Score: ' + str(int(Score)) + '      Max Speed: ' + str(max_speed), False, WHITE)
+        Screen.blit(your_score_text,(int(SCREEN_WIDTH/2-your_score_text.get_width()/2),int(SCREEN_HEIGHT/2 - your_score_text.get_height()/2)))
 
-        try_again_text = score_font.render('Play Again? Type Y/N', False, WHITE)
-        Screen.blit(try_again_text,(int((SCREEN_WIDTH-try_again_text.get_width())/2),int((SCREEN_HEIGHT + 3*game_over_text.get_height())/2)))
+        try_again_text = end_font.render('Play Again? Type Y/N', False, WHITE)
+        Screen.blit(try_again_text,(int((SCREEN_WIDTH-try_again_text.get_width())/2),int(SCREEN_HEIGHT/2 + space_between_text)))
 
         all_sprites.draw(Screen)
 
@@ -350,6 +434,7 @@ def main():
     pg.mixer.music.play(-1)
 
     while not quit:
+        begin_card(Screen)
         game_loop(Screen)
         quit = end_card(Screen)
     pg.quit()
